53cc2e1bac8db616f3783796e1e0598851da32b9
CrystalAngelLee <crystaljiaxuan@163.com>
2021-12-30 18:14:56 +0800

    webpack5 微前端实例

---
+++ READEME.md
@@ -0,0 +1,24 @@
+# Demo 说明
+
+当前项目中演示样例完整文档说明详见： 微前端
+
+项目指引
+
+- system 演示样例
+  - [crystal-systemjs](./crystal-systemjs)
+- single-spa 演示样例
+  - [container](./container)
+  - [noframe](./noframe)
+  - [reactframe](./reactframe)
+  - [vueframe](./vueframe)
+  - [navbar](./navbar)
+  - [tools](./tools)
+- 模块联邦演示样例
+  - [container](./fedcontainer)
+  - [products](./products)
+  - [cart](./cart)
+- 模块联邦完整实现方案
+  - [container](./mfcontainer)
+  - [auth](./auth)
+  - [dashboard](./dashboard)
+  - [marketing](./marketing)
--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -6,20 +6,17 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8081,
+    port: 8083,
   },
   plugins: [
     // 将 products 自身当做模块暴露出去
     new ModuleFederationPlugin({
       // 模块名称, 具有唯一性, 相当于 single-spa 中的组织名称
-      name: "products",
+      name: "cart",
       // 模块文件名称, 其他应用引入当前模块时需要加载的文件的名字
       filename: "remoteEntry.js",
       // 当前模块具体导出的内容
       exposes: {
-        // ./src/index => ./src/bootstrap 为什么 ?
-        // mount 方法是在 bootstrap.js 文件中导出的, 所以此处要导出 bootstrap
-        // 此处的导出是给容器应用使用的, 和当前应用的执行没有关系, 当前应用在执行时依然先执行 index
         "./Index": "./src/bootstrap",
       },
       shared: {
